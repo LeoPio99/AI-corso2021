d74febcb6e736b79782c1312a2c3b485e6af9357
LeoPio99 <95907406+LeoPio99@users.noreply.github.com>
2021-12-17 10:43:15 +0000

    .

--- src/main2.py
+++ src/main2.py
@@ -34,3 +34,29 @@ d={}
 print(d)
 my_dict={'nome': 'Leonardo', 'cognome': 'Piovan'}
 print(my_dict['nome'])
+
+number = 23
+guess=int(input('Enter an integer : '))
+if guess == number:
+  # New block starts hereprint('Congratulations, you guessed it.')
+   print('Congratulations, you guessed it(but you do not win any prizes!)')
+  # New block ends here
+elif guess < number:
+    # Another block
+     print('No, it is a little higher than that')
+else:
+       print('No, it is a little lower than that')# you must have guessed > number to reach here
+
+number = 23
+running = True
+while running:
+  guess = int(input('Enter an integer : '))
+  if guess == number:
+    print('Congratulations, you guessed it.')
+    # this causes the while loop to stop
+    running = False
+  elif guess < number:
+      print('No, it is a little higher than that.')
+  else:
+        print('No, it is a little lower than that.')
+       
