bf640984720fd6b261299b9e8368fa08c9b29db8
LeoPio99 <95907406+LeoPio99@users.noreply.github.com>
2022-01-14 10:30:44 +0000

    .

--- src/main2.py
+++ src/main2.py
@@ -2,8 +2,16 @@ class Person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
-    p1 = Person("John", 36)
-    print(p1.name)
-    print(p1.age)
+p1 = Person("John", 36)
+print(p1.name)
+print(p1.age)
 
-       
+import sys
+for arg in sys.argv[1:]:
+  try:
+    f = open(arg, 'r')
+  except OSError:
+      print('cannot open', arg)
+  else:
+      print(arg, 'has', len(f.readlines()), 'lines')
+      f.close()      
